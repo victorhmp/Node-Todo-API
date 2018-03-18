d24f60135e6af533d66545026731504f7df8c898
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-17 21:01:57 -0300

    Setup Express and create POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json()); // middleware
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text   
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+})
