747315661a9739d549035a2e76054cc500eed454
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-21 21:33:49 -0300

    Add GET /todos/:id route to fetch an individual todo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,32 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todos/122343 -> value
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    // validate ID
+      // 404 - send back empty send
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    
+    // findById
+      // sucess
+        // if todo - send it back
+        // if on todo - send back 404 with empty body
+      // error
+        // 400 - and send empty body back
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(3000, () => {
