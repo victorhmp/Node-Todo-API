45e6640e11873d6c62ae5569f8af7ee14cf96172
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-01 18:29:00 -0300

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -65,6 +67,32 @@ app.delete('/todos/:id', (req, res) => {
         res.status(200).send({todo});
     }).catch((e) => {
         return res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+
+        res.send(400).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
