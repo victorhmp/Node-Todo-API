44f7a40800d30ff9fd3f48a82b37b51e1394a16c
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-01 18:14:31 -0300

    Add tests for DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -41,12 +41,6 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send();
     }
     
-    // findById
-      // sucess
-        // if todo - send it back
-        // if on todo - send back 404 with empty body
-      // error
-        // 400 - and send empty body back
     Todo.findById(id).then((todo) => {
         if (!todo) {
             return res.status(404).send();
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,41 @@ describe('GET /todos/:id', () => {
             .end(done);
     })
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        let hexId = todos[1]._id.toHexString();
+
+        request(app)
+          .delete(`/todos/${hexId}`)
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.todo._id).toBe(hexId);
+          })
+          .end((error, resp) => {
+            if(error)
+                return done(err);
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist;
+                done();
+            }).catch((e) => done(e));
+          });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID('5ab2ef2b6912b5287eae1176').toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        let hexId = new ObjectID().toHexString();
+        
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+})
